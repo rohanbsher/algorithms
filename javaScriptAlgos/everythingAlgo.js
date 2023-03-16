62d8ceea0c02dd95a37a64ee942d9088d1bd88c1
Rohan Bhandari <bhandarirohan556@gmail.com>
2023-03-16 17:36:17 -0400

    Update everythingAlgo.js

--- javaScriptAlgos/everythingAlgo.js
+++ javaScriptAlgos/everythingAlgo.js
@@ -1,3 +1,465 @@
+/*
+Given a binary tree, return the deepest node (furthest away child). 
+
+Follow-up: If the Fellow did it recursively, ask them to do it iteratively or vice versa.
+
+
+EXAMPLE 1:
+    a
+   / \
+  b   c
+ /
+d
+
+returns d
+
+EXAMPLE 2:
+    a
+   / \
+  b   c
+
+returns b or c
+
+EXAMPLE 3:
+    a
+
+returns a
+
+EXAMPLE 4:
+    a - 1
+     \ 
+      b - 2
+       \
+        c - 3
+         \
+          d - 4
+
+returns d
+
+Edge cases:
+- One node tree: just return the root
+- Empty tree: invalid input, don't worry about it
+- Tree like a linked list
+
+
+Approach 1 - BFS:
+- Push each node from each level from top to bottom to queue
+- At each level, we push currNode to the nodes []
+while(queue.length > 0){
+  maxVal = queue[0]
+  queue.shift() // remove the first element 
+}
+- let maxVal = queue[0] // FIFO
+- At the end, we can return the last index of the nodes []
+
+Approach 2 (iterative):
+- Traverse through tree
+  - push node onto stack
+  - pop node from stack
+  - increment plus one
+  - push left and right children if not null
+  - Compare maxHeight with current height
+    - If greater
+      - store new height as maxHeight and node as maxNode
+- return maxNode
+
+
+
+FUNCTION SIGNATURE
+function deepestNode(root)
+
+EXAMPLE 1:
+    a
+   / \
+  b   c
+ /
+d
+
+returns d
+
+maxVal = d
+queue = []
+
+
+
+
+
+class Node {
+  constructor(val, left=null, right=null){
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+}
+
+function deepestNode(root) {
+  if (!root) return null;
+  let maxVal = null;
+  const queue = [root];
+  // queue = dequeue()
+  while (queue.length > 0) { // O(n) iterations of the loop
+    maxVal = queue[0];
+    const current = queue.shift(); // every shift is O(n)
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return maxVal;
+}
+
+// Time: O(n ^ 2)
+// Space: O(n)
+
+// <- nodes coming off the front || queue || <- nodes going in the back
+
+let tree = new Node('a', new Node('b', new Node('d')), new Node('c'))
+let tree1 = new Node('a', new Node('b'), new Node('c'))
+let tree2 = new Node('a')
+let tree3 = new Node('a', new Node('b', new Node('c', new Node('d'))))
+let tree4 = new Node('a',
+              new Node('b',
+                new Node("v"),
+                new Node('y')),
+                 
+              new Node('d'))
+
+console.log(deepestNode(null), null)
+console.log(deepestNode(tree), "d")
+console.log(deepestNode(tree1), "c")
+console.log(deepestNode(tree2), "a")
+console.log(deepestNode(tree3), "d")
+console.log(deepestNode(tree4), "y")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+'''
+❓ PROMPT
+We've been given a list of employees at the company, each person's seniority is denoted by a number, eg: Junior => 3, Senior => 5, and so on.
+
+For each person on the list, we want to find an adjacent employee that outranks them in the list to become their mentor. Return a new list of the same length corresponding to the level of the mentor.
+
+If someone can't find an adjacent superior, they should not be assigned a mentor so it should return their own level.
+
+Example(s)
+head = 3 -> 1 -> 3
+assignMentors(head) == [3,3,3]
+
+head = 5 -> 6 -> 9
+assignMentors(head) == [6,9,9]
+
+head = 2 -> 2 -> 2
+assignMentors(head) == [2,2,2]
+
+head = 2 -> 7 -> 4 -> 3 -> 5
+assignMentors(head) == [7,7,7,5,5]
+ 
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+0 1 0 -> 1 1 1
+1 5 1 -> 5 5 5
+5 1 1 -> 5 5 1
+1 2 3 -> 2 3 3
+
+Insightful & revealing test cases:
+// 4 5 6 7 8
+// 
+
+currNode = 4
+nextNode = 5
+
+if(currNode < nextNode) then update currNode
+else if (currNode > nextNode) the update nextNode
+
+we basically can see from the test case that as we go from left to right of the list
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1:
+Time: O(n) // every element in the list
+Space: O(1) // we're updating the list in place
+ 
+
+📆 PLAN
+Outline of algorithm #: 
+ 
+0 - 1 - 0 = 1 - 1 - 1
+
+currNode - 
+nextNode - 
+
+
+🛠️ IMPLEMENT
+function assignMentors(head) {
+def assignMentors(head: Node) -> list[int]:
+ 
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+'''
+
+
+class Node{
+  constructor(val, next=null){
+    this.val = val
+    this.next = next
+  }
+}
+
+// function assignMentors(head) {
+//   if(!head){
+//     return head
+//   }
+//   let arr = [head.val]
+
+//   function compareAdjacentMentors(head){
+//     if(!head.next){
+//       return
+//     }
+
+//     compareAdjacentMentors(head.next)
+
+//     // 0 1 0 0= 1 1 1
+//     // 1 1 1
+//     //[1, ]
+//     // console.log(head.val + "  --  " + head.next.val)
+//     if(head.val < head.next.val){
+//       head.val = head.next.val
+//       arr.push(head.next.val)
+//     } else if(head.next.val < head.val){
+//       head.next.val = head.val
+//       arr.push(head.val)
+//     }
+
+//   }
+
+//   compareAdjacentMentors(head)
+//   return arr
+// }
+
+
+function assignMentors(head) {
+  const results = []
+
+  const dummy = new Node(Number.MIN_VALUE, head)
+  let prev = dummy
+  let curr = head
+
+  while (curr) {
+
+    results.push(Math.max(
+      curr.val,
+      prev.val,
+      curr.next ? curr.next.val : Number.MIN_VALUE
+    ))
+
+    prev = curr
+    curr = curr.next
+  }
+
+  return results
+}
+
+console.log(assignMentors(null) === null || JSON.stringify(assignMentors(null)) =="[]")
+
+let head = new Node(1) // 1
+console.log(JSON.stringify(assignMentors(head)) == "[1]")
+
+head = new Node(3, new Node(1, new Node(3))) // 3 -> 1 -> 3
+console.log(JSON.stringify(assignMentors(head)))// == "[3,3,3]")
+
+head = new Node(5, new Node(6, new Node(9))) // 5 -> 6 -> 9
+console.log(JSON.stringify(assignMentors(head)))// == "[6,9,9]")
+
+head = new Node(2, new Node(2, new Node(2))) // 2 -> 2 -> 2
+console.log(JSON.stringify(assignMentors(head)))// == "[2,2,2]")
+
+// 2 -> 7 -> 4 -> 3 -> 5
+head = new Node(2, new Node(7, new Node(4, new Node(3, new Node(5)))))
+console.log(JSON.stringify(assignMentors(head)))// == "[7,7,7,5,5]")
+
+// 8 -> 7 -> 5 -> 4 -> 3
+head = new Node(8, new Node(7, new Node(5, new Node(4, new Node(3)))))
+console.log(JSON.stringify(assignMentors(head)) == "[8,8,7,5,4]")
+
+// 5 -> 6 -> 7 -> 8 -> 9
+head = new Node(5, new Node(6, new Node(7, new Node(8, new Node(9)))))
+console.log(JSON.stringify(assignMentors(head)) == "[6,7,8,9,9]")
+
+head = new Node(9, new Node(6, new Node(5))) // 9 -> 6 -> 5
+console.log(JSON.stringify(assignMentors(head)) == "[9,9,6]")
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+'''
+ Convert a sorted array into a balanced binary search tree.
+ 
+
+EXAMPLE(S)
+Input:  [1, 2, 3, 4, 5] =>
+Output:
+        3
+   2        5
+1        4
+
+or
+        3
+    2       4
+1              5
+
+or
+    3
+1       5
+  2   4
+
+or
+   3
+1     4
+   2     5
+ 
+ Considerations
+  - If input is empty, return null
+
+
+  [1, 2, 3, 4, 5]
+   L
+   M     
+   H
+
+
+              3
+           2
+         1
+  
+FUNCTION SIGNATURE
+function arrayToBST(arr) {
+def arrayToBST(arr: list[int]) -> Node:
+'''
+*/
+
+// class Node {
+//   constructor(val, left=null, right=null){
+//     this.val = val
+//     this.left = left
+//     this.right = right
+//   }
+// }
+
+// function arrayToBST2(arr) {
+//   if (!arr) {
+//     return null;
+//   }
+  
+//   const helper = (low, high) => {
+//     // base case
+//     if (low > high) {
+//       return null;
+//     }
+//     // calculate mid
+//     let mid = Math.floor((low + high) / 2);
+//     // initialize node with mid val
+//     let node = new Node(arr[mid]);
+//     node.left = helper(low, mid - 1)
+//     node.right = helper(mid + 1, high);
+
+//     return node;
+//   }
+
+//   return helper(0, arr.length - 1);
+// }
+
+// function arrayToBST(arr) {
+//   function helper(start, count) {
+//     if (count === 0) {
+//       return null;
+//     }
+
+//     const middle = start + Math.floor(count / 2);
+//     const root = new Node(arr[middle])
+//     root.left = helper(start, middle - start);
+//     root.right = helper(middle + 1, count - (middle - start + 1));
+
+//     return root;
+//   } 
+
+//   return helper(0, arr.length);
+// }
+
+
+// console.log(isCorrect(arrayToBST([])))
+// console.log(isCorrect(arrayToBST([1])))
+// console.log(isCorrect(arrayToBST([1,2])))
+// console.log(isCorrect(arrayToBST([1,2,3,4,5])))
+// console.log(isCorrect(arrayToBST([1,2,10,20,35,50,420,609])))
+// console.log(isCorrect(arrayToBST([-100,-50,-25,-20,-10,-1,0,1,2,10,20])))
+
+// /*******************************************/
+// /** This code is intentionally obfuscated **/
+// /*******************************************/
+// function isCorrect(r){return b(r)&&v(r);}
+// function gH(r){if(r == null)return 0;
+// let lH=gH(r.left);if(lH==-1)return -1;
+// let rH=gH(r.right);if(rH==-1)return -1;
+// if(Math.abs(lH-rH)>1)return -1;return Math.max(lH,rH)+1;}
+// function b(r){return gH(r)!=-1;}
+// function v(r){return v(r,Number.
+// MIN_SAFE_INTEGER,Number.MAX_SAFE_INTEGER);}
+// function v(r,m,M){if(r==null)return true;
+// if(r.val>=M||r.val<=m)return false;
+// return v(r.left,m,r.val)&&v(r.right,r.val,M);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 You are given the head of a linked list, and an integer k.
 
 Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
