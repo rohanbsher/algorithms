db08e502f32a2f47722067f83602656ca58bdc60
Rohan <bhandarirohan556@gmail.com>
2023-04-20 12:47:34 -0400

    Update everythingAlgo.js
    merge sort

--- javaScriptAlgos/everythingAlgo.js
+++ javaScriptAlgos/everythingAlgo.js
@@ -1,5 +1,65 @@
 /*
 
+function solution(array) {
+    
+    // merge sort - ascending order
+    // [6 5 4 3 2 1]
+    // [ 6 5 4] [3 2 1]
+    // [6] [5 4] [3] [2 1]
+    // [6] [5] [4] [3] [2] [1]
+    // [6] [4 5] [3] [1 2]
+    // [4 5 6] [1 2 3]
+    // [1 2 3 4 5 6]
+    
+    if(array.length <= 1){
+        return array
+    }
+    
+    let midPoint = Math.floor(array.length/2)
+    let leftArr = solution(array.slice(0, midPoint))
+    let rightArr = solution(array.slice(midPoint, array.length))
+    
+    return merge(leftArr, rightArr)
+    
+    function merge(leftArr, rightArr){
+        let arr3 = []
+        let leftIdx = 0
+        let rightIdx = 0
+        
+        while(leftArr.length > leftIdx && rightArr.length > rightIdx){
+            if(leftArr[leftIdx] <= rightArr[rightIdx]){
+                arr3.push(leftArr[leftIdx])
+                leftIdx++
+            } else {
+                arr3.push(rightArr[rightIdx])
+                rightIdx++
+            }
+        }
+        
+        return arr3.concat(leftArr.slice(leftIdx)).concat(rightArr.slice(rightIdx))
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Given a linked list, make a copy of the list from the kth position (from the head) to the end of the list.
 
 
