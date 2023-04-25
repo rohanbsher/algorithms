be58c166b4b1adced784706e4e86bda676cea491
Rohan <bhandarirohan556@gmail.com>
2023-04-25 09:23:02 -0400

    sum all the paths
    sum all the paths

--- javaScriptAlgos/everythingAlgo.js
+++ javaScriptAlgos/everythingAlgo.js
@@ -1,6 +1,141 @@
 
 /*
 
+class Node {
+  constructor(val, left=null, right=null){
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+}
+
+// Sum all the paths down till the leaf node
+
+function sumAllTreePathDigits(root, prev_total=0){
+  if(!root){
+    return 0
+  }
+
+  let curr_total = (prev_total * 10) + root.val
+  
+  if(!root.left && !root.right){
+    return curr_total
+  }
+
+  let left = sumAllTreePathDigits(root.left, curr_total)
+  let right = sumAllTreePathDigits(root.right, curr_total)
+  
+  return left + right
+}
+
+// Test Case 1: None
+console.log(sumAllTreePathDigits(null) === 0);
+
+// Test Case 2: One Node
+let one_node = new Node(5);
+console.log(sumAllTreePathDigits(one_node) === 5);
+
+// Test Case 3: One Path
+//        1
+//      1
+//    1
+//  1
+let one_path = new Node(1, new Node(1, new Node(1, new Node(1))));
+console.log(sumAllTreePathDigits(one_path) === 1111);
+
+// Test Case 4: Complete Tree
+//       1
+//    2      3
+//  4   5  6   7
+let complete_tree = new Node(1,
+  new Node(2,
+    new Node(4),
+    new Node(5)),
+  new Node(3,
+    new Node(6),
+    new Node(7)));
+console.log(sumAllTreePathDigits(complete_tree) === 522);
+
+// Test Case 5: Partial tree with 4 levels
+//        3
+//    2       4
+// 1    7     3  6
+//  5  3  8  2    0
+let partial_4_levels = new Node(3,
+  new Node(2,
+    new Node(1,
+      null,
+      new Node(5)),
+    new Node(7,
+      new Node(3),
+      new Node(8))),
+  new Node(4,
+    new Node(3,
+      new Node(2),
+      null),
+    new Node(6,
+      null,
+      new Node(0))));
+console.log(sumAllTreePathDigits(partial_4_levels) === 16658);
+
+// Test Case 6: Uneven Tree
+//        3
+//     2     4
+//  1   5      6
+//     3        8
+let uneven_tree = new Node(3,
+  new Node(2,
+    new Node(1),
+    new Node(5,
+      new Node(3))),
+  new Node(4,
+    null,
+    new Node(6,
+      null,
+      new Node(8))));
+console.log(sumAllTreePathDigits(uneven_tree) === 7042);
+
+// Test Case 7: Tree with only left child nodes
+//        4
+//      3
+//    2
+//  1
+let left_child_tree = new Node(4, new Node(3, new Node(2, new Node(1))));
+console.log(sumAllTreePathDigits(left_child_tree) === 4321);
+
+// Test Case 8: Tree with only right child nodes
+//  1
+//    2
+//      3
+//        4
+let right_child_tree = new Node(1, null, new Node(2, null, new Node(3, null, new Node(4))));
+console.log(sumAllTreePathDigits(right_child_tree) === 1234);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var maxPathSum = function(root) {
     let maxPathSum = -Infinity
 
